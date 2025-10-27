4ebd4ca5a3820714333998d7716dd9d8d77ce41b
potoole7 <paddy7wb@gmail.com>
2025-10-27 13:44:06 +0000

    Add all GPD functions and export

--- R/util.R
+++ R/util.R
@@ -58,3 +58,80 @@ rlaplace <- \(n) {
   u <- stats::runif(n)
   qlaplace(u)
 }
+
+#' @title Generalized Pareto Distribution density function
+#' @description PDF for Generalized Pareto Distribution (GPD).
+#' @param x Vector of quantiles.
+#' @param u Threshold parameter.
+#' @param sigma Scale parameter.
+#' @param xi Shape parameter.
+#' @return Vector of density values.
+#' @rdname dgpd
+#' @export
+dgpd <- \(x, u, sigma, xi) {
+  if (abs(xi) > 1e-6) {
+    dens <- (1 / sigma) * (1 + xi * (x - u) / sigma)^(-1 / xi - 1)
+    dens[x < u] <- 0
+    dens[(1 + xi * (x - u) / sigma) <= 0] <- 0
+    return(dens)
+  } else {
+    dens <- (1 / sigma) * exp(-(x - u) / sigma)
+    dens[x < u] <- 0
+    return(dens)
+  }
+}
+
+#' @title Generalized Pareto Distribution cumulative distribution function
+#' @description CDF for Generalized Pareto Distribution (GPD).
+#' @param q Vector of quantiles.
+#' @param u Threshold parameter.
+#' @param sigma Scale parameter.
+#' @param xi Shape parameter.
+#' @return Vector of CDF values.
+#' @rdname pgpd
+#' @export
+pgpd <- \(q, u, sigma, xi) {
+  if (abs(xi) > 1e-6) {
+    cdf <- 1 - (1 + xi * (q - u) / sigma)^(-1 / xi)
+    cdf[q < u] <- 0
+    cdf[(1 + xi * (q - u) / sigma) <= 0] <- 1
+    return(cdf)
+  } else {
+    cdf <- 1 - exp(-(q - u) / sigma)
+    cdf[q < u] <- 0
+    return(cdf)
+  }
+}
+
+#' @title Generalized Pareto Distribution quantile function
+#' @description Quantile function for Generalized Pareto Distribution (GPD).
+#' @param p Vector of probabilities.
+#' @param u Threshold parameter.
+#' @param sigma Scale parameter.
+#' @param xi Shape parameter.
+#' @return Vector of quantiles.
+#' @rdname qgpd
+#' @export
+qgpd <- \(p, u, sigma, xi) {
+  if (abs(xi) > 1e-6) {
+    return(u + (sigma / xi) * ((1 - p)^(-xi) - 1))
+  } else {
+    return(u - sigma * log(1 - p))
+  }
+}
+
+#' @title Generalized Pareto Distribution random generation
+#' @description Generate random samples from Generalized Pareto Distribution
+#' (GPD).
+#' @param n Number of samples to generate.
+#' @param u Threshold parameter.
+#' @param sigma Scale parameter.
+#' @param xi Shape parameter.
+#' @return Vector of random samples from GPD.
+#' @rdname rgpd
+#' @export
+rgpd <- \(n, u, sigma, xi) {
+  p <- stats::runif(n)
+  # use qgpd to get samples
+  qgpd(p, u, sigma, xi)
+}
